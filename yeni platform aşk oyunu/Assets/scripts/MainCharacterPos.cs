85b92c93ab27c26f80141c9533cd127e1f3d4213
kirazzobu16 <37017093+kirazzobu16@users.noreply.github.com>
2021-03-08 15:52:32 +0300

    ölüm kısmı iyileştirildi

--- yeni platform aşk oyunu/Assets/scripts/MainCharacterPos.cs
+++ yeni platform aşk oyunu/Assets/scripts/MainCharacterPos.cs
@@ -1,42 +1,51 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public class MainCharacterPos : MonoBehaviour
 {
     private GameMaster gm;
     Animator animator;
-    
-    
+  
+
     void Start()
     {
 
         animator = GetComponent<Animator>();
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
         transform.position = gm.lastCheckPointPos;
-        
-        animator.SetBool("IsDead", false);
+
+
     }
-  
-    
-   
+
 
     // Update is called once per frame
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.tag == "dead")
-        {
-            
-            // GetComponent<player>().enabled = false;
-          
+       
+        
 
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            
+            // GetComponent<player>().enabled = false;
+            if (other.tag == "dead")
+            {
+            StartCoroutine(RespawnCoroutine());
+            }
 
         }
-    
+    public IEnumerator RespawnCoroutine()
+    {
+        animator.SetBool("IsJumping", false);
+        animator.SetBool("IsDead", true);
+   
+        yield return new WaitForSeconds(0.30f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-
 }
+   
+
+
+    
